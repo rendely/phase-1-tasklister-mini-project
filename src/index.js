d1ab18fecf3e4c08c819f9cc126caebf9a550eed
matthew rendely <matthew.rendely@gmail.com>
2023-03-17 07:21:59 -0700

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //EL on submit button prevents default and appends child to-do if not empty
+  document.querySelector('form').addEventListener('submit', e=>{
+    e.preventDefault();
+    const todo = e.target['new-task-description'].value;
+    //Only add if not empty
+    todo.length > 0 ? addTodo(todo) : console.log('empty');
+    //Clear input after
+    e.target['new-task-description'].value = '';
+  })
 });
+
+// Add to do to list
+function addTodo(todo){
+  console.log(todo);
+  const taskList = document.querySelector('#tasks');
+  const newTodo = document.createElement('li');
+  newTodo.textContent = todo;
+  taskList.appendChild(newTodo);
+}
+
